d549da6cafab6676e1591c8fcb7c3e2a87cc57c3
Kings Israel <milimokings@gmail.com>
2020-09-21 13:22:08 +0300

    Blog react mobile app

---
+++ src/context/BlogContext.js
@@ -0,0 +1,63 @@
+import React, { useReducer, useState } from 'react';
+import { call } from 'react-native-reanimated';
+import jsonServer from '../api/jsonServer';
+import createDataContext from './createDataContext';
+
+const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'get_blogposts':
+            return action.payload;
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost
+            });
+        case 'delete_blogpost':
+            return state.filter(blogPost => blogPost.id !== action.payload);
+        default:
+            return state;
+    }
+}
+
+const getBlogPosts = (dispatch) => {
+    return async () => {
+        const response = await jsonServer.get('/blogposts');
+
+        dispatch({type: 'get_blogposts', payload: response.data});
+    }
+}
+
+const addBlogPost = (dispatch) => {
+    return async (title, content, callback) => {
+        await jsonServer.post('/blogposts', { title, content });
+        if(callback){
+            callback();
+        }
+    }
+}
+
+const deleteBlogPost = (dispatch) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`);
+        dispatch({ type: 'delete_blogpost', payload: id});
+    }
+}
+
+const editBlogPost = (dispatch) => {
+    return async (title, content, id, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, {title, content});
+        dispatch({type: 'edit_blogpost', payload: {title, content, id}});
+        if(callback){
+            callback();
+        }
+    }
+}
+
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+    []
+);
+
+export default BlogContext;
